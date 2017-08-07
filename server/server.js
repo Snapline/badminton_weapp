49b7b4b5eaa075000361f17342d5d30032f16511
gaoyang <gaoyang@think-land.com>
2017-08-07 18:20:12 +0800

    daily commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var data = require('./data.json')
+
+
+app.get('/creatematch', function (req, res) {
+  res.json(data.createMatch );
+})
+
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
